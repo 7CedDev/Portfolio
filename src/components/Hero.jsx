10822a1b2468a91314bf96b9e255e812b4571aac
7CedDev <cedricrosine94@gmail.com>
2023-03-20 14:38:11 +0100

    Fin du composant Hero en responsive avec animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,12 +18,35 @@ const Hero = () => {
         text-white`}>Hello, Je suis <span className='text-[#915eff]'>Cédric</span></h1>
         <p className={`${styles.heroSubText}mt-2
         text-xhite-100`} >
-          Je développe des sites web dynamiques, des applications, <br className='sm:block hidden' /> en Front et Back
+          Je développe des sites web et des applications dynamiques, <br className='sm:block hidden' /> en Front et Back
         </p>
       </div>
       </div>
 
       <ComputersCanvas />
+
+      <div className='absolute xs:bottom-2 bottom-4 w-full 
+      flex justify-center items-center'>
+        <a href="#about">
+          <div className='w-[35px] h-[64px]
+          rounded-3xl border-4 border-secondary
+          flex justify-center items-start p-2'>
+            <motion.dev 
+            animate={{
+              y: [0,24, 0]
+            }}
+            transition={{
+              duration: 1.5,
+              repeat: Infinity,
+              repeatType: 'loop'
+            }}
+            className='w-3 h-3 rounded-full
+            bg-secondary mb-1'
+            />
+          </div>
+        </a>
+        
+      </div>
     </section>
   )
 }
